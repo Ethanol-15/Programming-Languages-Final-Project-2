3dd1fc2d2c4c7624fa81044e83ee79a89ad31718
PailanJoaquin <joaquinantoine.pailan.cics@ust.edu.ph>
2024-12-10 18:43:32 +0800

    Delete src directory
    I changed some parts to include payment integration

--- src/main/java/com/cpo/syntax_checker/SyntaxCheckerServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package com.cpo.syntax_checker;
-
-public interface SyntaxCheckerServiceInterface {
-    String checkSyntax(SyntaxCheckerRequest request); // Abstract method for syntax checking
-    int getTotalCalls(); // Abstract method to get total calls
-    void resetTotalCalls(); // Abstract method to reset calls
-}
